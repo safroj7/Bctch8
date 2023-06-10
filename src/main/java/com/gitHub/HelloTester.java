e1486c2c26dfcc6a6c5fb6ebb5580d4c5d50dd58
kj64778 <kj64778@gmail.com>
2023-06-10 09:23:26 +0530

    Add new class

---
+++ src/main/java/com/gitHub/HelloTester.java
@@ -0,0 +1,12 @@
+package com.gitHub;
+
+public class HelloTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Tester!!");
+
+	}
+
+}
